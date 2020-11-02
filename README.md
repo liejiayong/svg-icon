3067a9fe186e5b5daba829ae1ea180eba63e0e1f
liejiayong <809206619@qq.com>
2020-11-02 16:31:42 +0800

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# svg-icon
+
+## Usage
+
+- 引入模块
+
+```js
+import Vue from 'vue';
+import SvgIcon from 'vue-svg';
+Vue.use(SvgIcon, { name: 'svgIcon' });
+```
+
+- icon-class 可以传入 svg 名称，或 http 地址图片
+
+```html
+<svg-icon :icon-class="'home'"></svg-icon>
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+import SvgIcon from './icon.vue';
+
+const iconSymbol = 'svgIcon';
+SvgIcon.name = iconSymbol;
+
+// use object to export
+const JYSvgIconPlug = {
+  install: function (Vue, opts = { name: iconSymbol, svgRequire: '' }) {
+    SvgIcon.name = opts.name;
+    Vue.component(opts.name, SvgIcon);
+  },
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.component(iconSymbol, SvgIcon);
+}
+
+export { SvgIcon, iconSymbol };
+
+export default JYSvgIconPlug;
